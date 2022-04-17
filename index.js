9f25cf51ee7aab22653f56fcf00b3139c0e9385a
Vikram Singh <101462288+vikrams788@users.noreply.github.com>
2022-04-17 15:37:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var randomNumber1=Math.floor(Math.random()*6)+1;
-var randomImageSource="images/dice"+randomNumber1+".png";
+var randomImageSource="dice"+randomNumber1+".png";
 document.querySelectorAll("img")[0].setAttribute("src",randomImageSource);
 
 var randomNumber2=Math.floor(Math.random()*6)+1;
-var randomImageSource2="images/dice"+randomNumber2+".png";
+var randomImageSource2="dice"+randomNumber2+".png";
 document.querySelectorAll("img")[1].setAttribute("src",randomImageSource2);
 
 if(randomNumber1>randomNumber2){
